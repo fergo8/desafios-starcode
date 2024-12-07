0dee94da1ec5281e3555344d89d6969d3d8f511e
Jefferson Gomes <30440307+fergo8@users.noreply.github.com>
2024-12-07 01:45:27 -0300

    First commit

---
+++ src/main/java/com/desafiosstarcode/desafios_starcode/DesafiosStarcodeApplication.java
@@ -0,0 +1,13 @@
+package com.desafiosstarcode.desafios_starcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosStarcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiosStarcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiosstarcode/desafios_starcode/DesafiosStarcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiosstarcode.desafios_starcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosStarcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
